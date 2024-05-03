a58a5ccfd89d914c3b642d617b0eb9e181d8bb42
allenJames7777 <alluvido_200000000403@uic.edu.ph>
2024-05-03 15:02:32 +0800

    arc patabang

--- src/app/landingpage/Contact.tsx
+++ src/app/landingpage/Contact.tsx
@@ -6,31 +6,34 @@ import { Button } from "@/components/ui/button";
 const Contact = () => {
   return (
     <div className="select-none">
+      
       <section className="mb-10">
         <div className="flex flex-col items-center pt-11">
-          <div className="bg-blue-400 w-[1100px] h-[282px] rounded-sm flex items-center text-nowrap drop-shadow-2xl pt-20 ml-7">
-            <div className="text-white text-4xl font-extrabold ml-9 mb-24">
+          <div className="bg-blue-400 w-[395px] sm:w-[620px] lg:h-[182px] lg:w-[820px] 2xl:w-[1020px] 2xl:h-[210px] h-[250px] rounded-sm flex  drop-shadow-lg pt-16 justify-center items-center">
+            {" "}
+            {/* Added justify-center and items-center */}
+            <div className="text-white text-2xl font-extrabold mb-24 ml-6 items-start lg:pt-10">
               SUBSCRIBE TO NEWSLETTER
-              <p className="text-base mt-4 font-normal">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              <p className="text-sm mt-4 font-normal max-w-96">
+                Lorem ipsum dolor sit amet consectetur. Cum dui mattis cursus
+                neque.
               </p>
-              <div className="flex w-[400px] h-[38px] space-x-2 text-black font-normal mt-5">
-                <Input type="email" placeholder="Email" />
-                <Button className="bg-blue-600" type="submit">
+              <div className="flex flex-col lg:flex lg:flex-row items-center w-[300px] h-[38px] space-x-2 text-black font-normal mt-5 ml-5 lg:ml-5 sm:ml-6">
+                <Input
+                  type="email"
+                  placeholder="Email"
+                  className="w-[380px] ml-2 sm:w-[550px] lg:w-[256px]"
+                />
+                <Button
+                  className="bg-blue-600 w-[380px] mt-2 lg:mb-4 sm:mt-4 sm:w-[550px] lg:w-[156px]"
+                  type="submit"
+                >
                   Subscribe
                 </Button>
               </div>
             </div>
-            <div className="flex flex-col mr-5 pb-[124px]">
-              <Image
-                src="/subscribe.svg"
-                alt="icon"
-                width={510}
-                height={500}
-                style={{
-                  maxWidth: "100%",
-                  height: "auto",
-                }} />
+            <div className=" flex-col lg:mt-10 2xl:ml-16 pb-[124px] hidden lg:block">
+              <Image src="/subscribe.svg" alt="icon" width={500} height={235} />
             </div>
           </div>
         </div>
